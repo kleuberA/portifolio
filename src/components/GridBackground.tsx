35af9ac610e71a386f2583c942e3fd74061db597
Kleuber Silva <kleuber20@hotmail.com>
2024-03-02 11:11:13 -0300

    Fix responsive title page

--- src/components/GridBackground.tsx
+++ src/components/GridBackground.tsx
@@ -65,7 +65,7 @@ export function GridBackground() {
                     <path d="M4.708 5.578L2.061 8.224l2.647 2.646-.708.708-3-3V7.87l3-3 .708.708zm7-.708L11 5.578l2.647 2.646L11 10.87l.708.708 3-3V7.87l-3-3zM4.908 13l.894.448 5-10L9.908 3l-5 10z" />
                 </motion.svg>
                 <TextGenerateEffect words={name} className="text-xl" />
-                <TextGenerateEffect words={words} className="text-5xl" />
+                <TextGenerateEffect words={words} className="text-xl lg:text-5xl" />
             </div>
         </div>
     );
