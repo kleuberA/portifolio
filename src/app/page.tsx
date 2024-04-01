ceba5dbd429e5f301bd3be3b1127e1aa4f5b243a
Kleuber Silva <kleuber20@hotmail.com>
2024-04-01 18:20:36 -0300

    Remove GridBackground component from Home page
    The GridBackground component has been removed from the Home page. The associated CSS and icons have also been removed. The Logo component has been updated to display the full name of the developer instead of the initials.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import { GridBackground } from "@/components/GridBackground";
 import About from "@/components/about/about";
 import Contact from "@/components/contact/Contact";
 import Projects from "@/components/projects/Projects";
@@ -6,7 +5,7 @@ import Projects from "@/components/projects/Projects";
 export default function Home() {
   return (
     <div className="">
-      <GridBackground />
+
       <About />
       <Projects />
       <Contact />
--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -1,12 +1,7 @@
-import { ChevronLeftIcon, ChevronRightIcon, SlashIcon } from "@radix-ui/react-icons"
-
 export default function Logo() {
     return (
         <section className="flex flex-row items-center justify-center hover:bg-secondary/50 hover:text-primary text-accent-foreground transition-all duration-200 cursor-pointer p-2 rounded-sm border border-transparent hover:border-border">
-            <ChevronLeftIcon width={30} height={30} />
-            <h1 className="font-bold text-xl">KS</h1>
-            <SlashIcon width={15} height={15} />
-            <ChevronRightIcon width={30} height={30} />
+            <h1 className="font-bold text-xl">Kleuber Silva</h1>
         </section>
     )
 }
