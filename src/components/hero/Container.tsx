36e23f35d5b0dc581c92ed7f0ceb6583216104df
Kleuber Silva <kleuber20@hotmail.com>
2024-04-09 13:37:31 -0300

    "Added ContainerHero component to Hero section"
    This commit adds the `ContainerHero` component to the `Hero` section, which already contains the `Menu` component. The exact implementation and purpose of the `ContainerHero` component are not specified in the provided code diff.

---
+++ src/components/hero/Container.tsx
@@ -0,0 +1,32 @@
+"use client"
+import Image from "next/image";
+import Computer from '../../../public/computer.png';
+import { motion } from "framer-motion";
+
+export default function ContainerHero() {
+    return (
+        <section className="w-[80dvw] min-h-[80dvh] flex flex-row mx-auto items-center">
+            <div className="flex flex-col gap-2">
+                <motion.h1
+                    initial={{ opacity: 0, x: -100 }}
+                    animate={{ opacity: 1, x: 0 }}
+                    transition={{ duration: 1.5 }}
+                    className="text-3xl w-[35dvw]">
+                    Olá, sou o Kleuber Silva, um desenvolvedor full stack.
+                </motion.h1>
+                <motion.span
+                    initial={{ opacity: 0, y: 50 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    transition={{ duration: 1.7 }}
+                    className="text-sm">Formado em Ciência da Computação pela UNIP.</motion.span>
+            </div>
+            <motion.div
+                initial={{ opacity: 0, x: 100 }}
+                animate={{ opacity: 1, x: 0 }}
+                transition={{ duration: 1.5 }}
+                className="w-full flex items-center justify-center">
+                <Image src={Computer} alt={""} />
+            </motion.div>
+        </section>
+    )
+}
--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,9 +1,11 @@
 import Menu from "../menu";
+import ContainerHero from "./Container";
 
 export default function Hero() {
     return (
         <section className="w-full min-h-screen">
             <Menu />
+            <ContainerHero />
         </section>
     )
 }
