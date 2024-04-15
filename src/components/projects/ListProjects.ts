50d64a06baaa0dde2d9bbc8900f869160ae7cbd8
Kleuber Silva <kleuber20@hotmail.com>
2024-04-14 21:38:03 -0300

    "Update ListProjects with new project and reorder"
    This commit updates the `ListProjects` array by adding a new project "GatherUs" and reordering the elements. The existing projects "My Chat Go" and

--- src/components/projects/ListProjects.ts
+++ src/components/projects/ListProjects.ts
@@ -1,17 +1,26 @@
-export const ListProjects = [{
-    title: "My Chat Go",
-    description: "It is a chat application where several users can enter a certain room and send their message, and whoever is in the same room will be able to see the messages sent.",
-    technologies: ["React", "TypeScript", "TailwindCSS", "Framer Motion", "Supabase", "Socket.io", "NestJS", "WebSocket"],
-    image: "https://imgur.com/N6TFcLq.png",
-    url: "",
-    repo: "https://github.com/kleuberA/mychatgo",
-},
-{
-    title: "My Notes Go",
-    description: "This application was developed to make it easier for users to take notes, in other words, a place where they can quickly write something down and save it so that there is no risk of forgetting it.",
-    technologies: ["React", "TypeScript", "TailwindCSS", "Framer Motion", "Supabase", "React Hot Toast", "React-query"],
-    image: "https://imgur.com/8shKADu.png",
-    url: "",
-    repo: "https://github.com/kleuberA/my-notes-go",
-},
+export const ListProjects = [
+    {
+        title: "GatherUs",
+        description: "This application is a social network in which only the front-end was created, that is, it does not yet have a back-end. It is a simple social network like all the others, the difference being the design that was applied in the development of this application, using the ShadCn lib to carry out the design, it is an application made only for the web, no adaptation or responsiveness of the system was made to it. be compatible with smartphones.",
+        technologies: ["ReactJs", "TypeScript", "TailwindCSS", "NextJs", "ShadCn", "Phosphor Icons"],
+        image: "https://imgur.com/XqcBBRH.png",
+        url: "",
+        repo: "https://github.com/kleuberA/GatherUS",
+    },
+    {
+        title: "My Chat Go",
+        description: "It is a chat application where several users can enter a certain room and send their message, and whoever is in the same room will be able to see the messages sent.",
+        technologies: ["React", "TypeScript", "TailwindCSS", "Framer Motion", "Supabase", "Socket.io", "NestJS", "WebSocket"],
+        image: "https://imgur.com/N6TFcLq.png",
+        url: "",
+        repo: "https://github.com/kleuberA/mychatgo",
+    },
+    {
+        title: "My Notes Go",
+        description: "This application was developed to make it easier for users to take notes, in other words, a place where they can quickly write something down and save it so that there is no risk of forgetting it.",
+        technologies: ["React", "TypeScript", "TailwindCSS", "Framer Motion", "Supabase", "React Hot Toast", "React-query"],
+        image: "https://imgur.com/8shKADu.png",
+        url: "",
+        repo: "https://github.com/kleuberA/my-notes-go",
+    },
 ];
